bcc5799f9ba9aad526c4400a5dc89554e5bb84c0
nahathaimays <eeyns6@nottingham.ac.uk>
2021-12-14 21:27:51 +0000

    Removed lib, obj file Worksheet3_4_5

---
+++ Worksheet5/exercise2/main.cpp
@@ -0,0 +1,11 @@
+// exercise2/main.cpp ------------------------------------------------
+#include "mainwindow.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
+}
+// ---------------------------------------------------------------------
