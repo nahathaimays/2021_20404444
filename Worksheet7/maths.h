2b24a348ae4e3c54cb9f0fd1ffe0959ff0b9b8cc
nahathaimays <eeyns6@nottingham.ac.uk>
2022-04-04 10:22:54 +0100

    added Worksheet7

---
+++ Worksheet7/maths.h
@@ -0,0 +1,16 @@
+// maths.h
+
+#ifndef MATHSLIB_MATHS_H
+#define MATHSLIB_MATHS_H
+
+/** @file
+* This file contains the declarations of all exported functions.
+*/
+
+/** Brief description
+* This description will hopefully be picked up on by doxygen
+*/
+
+int add(int a, int b);
+
+#endif
